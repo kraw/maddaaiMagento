6300731b8c82d11ac9caa260888520d12c246554
george <kraw65@gmail.com>
2018-03-14 11:26:58 +0100

    Fix menu glich

--- code/Syrus/Navigation/Plugin/Topmenu.php
+++ code/Syrus/Navigation/Plugin/Topmenu.php
@@ -38,45 +38,45 @@ class Topmenu
 
     public function afterGetHtml(\Magento\Theme\Block\Html\Topmenu $topmenu, $html){
 
-        $html .= "<li class=\"level0 nav-6 last level-top\">";
-        $html .= "<a href=\"" . "/" . "\" class=\"level-top\" >" .
+        $html .= "<li class=\"level0 nav-6 level-top\">";
+        $html .= "<a href=\"/\" class=\"level-top\" >" .
             "<span>" . __("Home") . "</span>" .
             "</a>";
         $html .= "</li>";
-        $html .= "<li class=\"level0 nav-6 last level-top\">";
-        $html .= "<a  href=\"" . "/categorie" . "\"   class=\"level-top\" " .
-            "<span data-toggle=\"collapse\" >" . __("Categorie") . "</span>" .
+        $html .= "<li class=\"level0 nav-6 level-top\">";
+        $html .= "<a  href=\"/categorie\" class=\"level-top\" >" .
+            "<span  title=\"Elenco categorie\">" . __("Categorie") . "</span>" .
             "</a>";
         $html .= "</li>";
-        $html .= "<li class=\"level0 nav-6 last level-top\">";
-        $html .= "<a href=\"" . "/in-scadenza" . "\" class=\"level-top\" >" .
+        $html .= "<li class=\"level0 nav-6 level-top\">";
+        $html .= "<a href=\"/in-scadenza\" class=\"level-top\" >" .
             "<span  title=\"Gruppo di acquisti\">" . __("GDA in scadenza") . "</span>" .
             "</a>";
         $html .= "</li>";
 
-        $html .= "<li class=\"level0 nav-6 last level-top\">";
-        $html .= "<a href=\"" . "/conclusi" . "\" class=\"level-top\" >" .
+        $html .= "<li class=\"level0 nav-6 level-top\">";
+        $html .= "<a href=\"/conclusi\" class=\"level-top\" >" .
             "<span  title=\"Gruppo di acquisti\">" . __("GDA conclusi") . "</span>" .
             "</a>";
         $html .= "</li>";
 
-        $html .= "<li class=\"level0 nav-6 last level-top\">";
+        $html .= "<li class=\"level0 nav-6 level-top\">";
         $html .= "<a href=\"" . "/bidrequest/bidrequest/form" . "\" class=\"level-top\" >" .
             "<span  title=\"Gruppo di acquisti\">" . __("Crea il tuo GDA") . "</span>" .
             "</a>";
         $html .= "</li>";
 
-        $html .= "<li class=\"level0 nav-6 last level-top\"> ";
-        $html .= "<a href=\"" . "/valutation-products" . "\" class=\"level-top\" >" .
+        $html .= "<li class=\"level0 nav-6 level-top\"> ";
+        $html .= "<a href=\"/valutation-products\" class=\"level-top\" >" .
             "<span  title=\"Gruppo di acquisti\">" . __("GDA proposte") . "</span>" .
             "</a>";
         $html .= "</li>";
-        $html .= "<li class=\"level0 nav-6 last level-top\" style=\"float:right\">".
-            "<a class=\"level-top\" href=\"" . "/customer/account/" . "\"><span>". __("Il mio account") ."</span>".
+        $html .= "<li class=\"level0 nav-6 level-top\" style=\"float:right\">".
+            "<a class=\"level-top\" href=\"/customer/account/\"><span>". __("Il mio account") ."</span>".
             "</a>";
         $html .= "<ul class=\"level1 submenu\" >";
-        $html .= "<li class=\"level2 nav-3-1-2\"><a href=\"" . "/index.php/preorder/gda/index/" . "\">" . __("I miei GDA") . "</a></li>";
-        $html .= "<li class=\"level2 nav-3-1-2\"><a href=\"" . "/customer/account/logout/" . "\">" . __("Esci") . "</a></li>";
+        $html .= "<li class=\"level2 nav-3-1-2\"><a href=\"/index.php/preorder/gda/index/\">" . __("I miei GDA") . "</a></li>";
+        $html .= "<li class=\"level2 nav-3-1-2\"><a href=\"/customer/account/logout/\">" . __("Esci") . "</a></li>";
         $html .="</ul>";
         $html .="</li>";
 
